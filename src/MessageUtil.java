b550482cc0975089c9cc194bd5ae73302e6a49f0
赵光亚 <zhaoguangya@apexsoft.com.cn>
2019-02-15 11:44:47 +0800

    Test Junit With Ant

---
+++ src/MessageUtil.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Clancey on 2019/2/15.
+ */
+public class MessageUtil {
+    private String message;
+
+    //Constructor
+    //@param message to be printed
+    public MessageUtil(String message){
+        this.message = message;
+    }
+
+    // prints the message
+    public String printMessage(){
+        System.out.println(message);
+        return message;
+    }
+
+    // add "Hi!" to the message
+    public String salutationMessage(){
+        message = "Hi!" + message;
+        System.out.println(message);
+        return message;
+    }
+}
---
+++ src/TestMessageUtil.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by Clancey on 2019/2/15.
+ */
+public class TestMessageUtil {
+    String message = "Robert";
+    MessageUtil messageUtil = new MessageUtil(message);
+
+    @Test
+    public void testPrintMessage() {
+        System.out.println("Inside testPrintMessage()");
+        assertEquals(message,messageUtil.printMessage());
+    }
+
+    @Test
+    public void testSalutationMessage() {
+        System.out.println("Inside testSalutationMessage()");
+        message = "Hi!" + "Robert";
+        assertEquals(message,messageUtil.salutationMessage());
+    }
+}
